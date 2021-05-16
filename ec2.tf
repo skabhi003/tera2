426bfd8e2d68fbdb66699ff941e48392fac96a2d
skabhi003 <67892083+skabhi003@users.noreply.github.com>
2021-05-16 13:36:44 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,14 @@ provider "aws" {
   region     = "ap-south-1"
   profile    = "terraform"
 }
+
+resource "aws_instance" "os1" {
+  ami           = "ami-010aff33ed5991201"
+  instance_type = "t2.micro"
+  key_name = "hadoopkey"
+  vpc_security_group_ids = [ "sg-0e818dd7bf77300fb" ]
+  tags = {
+    Name = "My TF Ins"
+  }
+}
+
